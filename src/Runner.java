6c76d63ec8a399ca0a2477f1936f62ffccc59cc4
ozan-k <ozan.kah@gmail.com>
2023-03-21 09:44:53 +0100

    exercise6

--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,10 @@
 class Runner {
 
   public static void main(String[] args) {
-    InverseString inv1 = new InverseString("Tiago");
+    InverseString inv1 = new InverseString("John");
     System.out.println(inv1);
 
-    InverseString inv2 = new InverseString("Davi");
+    InverseString inv2 = new InverseString("Jane");
     System.out.println(inv2);
 
     System.out.println("charAt(2) = " + inv2.charAt(2));
